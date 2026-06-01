b5bab9879b65740bfdcc9b4b18dc49940563e071
Sanjay Kumar <coderdevil39@gmail.com>
2026-06-01 15:16:24 +0530

    Failed free domain

--- backend-updated/src/main/java/com/college/gatepass/config/SecurityConfig.java
+++ backend-updated/src/main/java/com/college/gatepass/config/SecurityConfig.java
@@ -163,7 +163,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",                          // local dev
-                "https://gatepass.cc.cd"                // ✅ YOUR NEW FRONTEND
+                "https://digital-gate-pass.onrender.com"                // ✅ YOUR NEW FRONTEND
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
